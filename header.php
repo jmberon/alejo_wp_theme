4787b206718d16332837cc9ecb63658a0a0580cc
Jhoan Beron <jb@spenda.co>
2022-10-15 16:42:30 +0800

    Remove title and tagline

--- header.php
+++ header.php
@@ -15,22 +15,18 @@
 </head>
 
 <body>
-    <header>
-        <div class ="container">
-            <h1><?php bloginfo('name'); ?></h1>
-            <h2><?php bloginfo('description'); ?></h2>
-        </div>
+    <header>        
+        <nav class="main-nav">
+            <div class="container">
+                <?php 
+                    $args = array(
+                    'theme_location' => 'primary'
+                    ); 
+                ?>
+            <?php wp_nav_menu($args); ?>
+            </div>
+        </nav>
     </header>
-    <nav class="main-nav">
-        <div class="container">
-            <?php 
-                $args = array(
-                'theme_location' => 'primary'
-                ); 
-            ?>
-        <?php wp_nav_menu($args); ?>
-        </div>
-    </nav>
     
     <!-- HERO  -->
     <div class="hero-section">
