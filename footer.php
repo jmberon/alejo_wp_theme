56806840358b2e4d881e6aa3f0fd57c1ac4ad79e
Jhoan Beron <jb@spenda.co>
2022-10-15 16:50:34 +0800

    Remove sidebar

--- footer.php
+++ footer.php
@@ -1,10 +1,12 @@
-<div class="sidebar">
+<?php 
+/* <div class="sidebar">
          <?php if(is_active_sidebar('sidebar')) : ?>
             <?php dynamic_sidebar('sidebar'); ?>
          <?php endif; ?>
       </div>
       <div class="clr"></div>
-      </div>
+      </div>*/
+?>
 
     <footer>
       <p>Jerry Verschoor Portfolio <br> jeroen.g.verschoor@gmail.com <br> 2022</p>
